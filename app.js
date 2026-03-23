ce0323b20efecc5ff669207cdac0f34cbced0f91
BlueWhale <wesley.eller@gmail.com>
2026-03-23 23:03:33 +0930

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const list = document.getElementById('list');
-DATA.forEach(item=>{
-  const div=document.createElement('div');
-  div.className='card';
-  div.innerHTML=`<h3>${item.title}</h3>
-  <p>${item.author}</p>
-  <a href="detail.html?id=${item.id}">Open</a>`;
-  list.appendChild(div);
-});
